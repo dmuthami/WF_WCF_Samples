57ca87cf2322af693f2631f46b07eac2dc742c16
David Muthami <waruid@gmail.com>
2016-02-19 13:18:54 +0300

    Added files via upload

---
+++ WCFServiceLibrary/CalculatorService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WCFServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class CalculatorService : ICalculator
+    {
+        public IAsyncResult BeginAddMethod(double n1, double n2, AsyncCallback callback, object asyncState)
+        {
+            var task = Task<double>.Factory.StartNew((res) => MyBeginAddMethod(asyncState, n1, n2), asyncState);
+            return task.ContinueWith(res => callback(task));
+
+        }
+        public double EndAddMethod(IAsyncResult result)
+        {
+            return ((Task<double>)result).Result;
+        }
+
+        private double MyBeginAddMethod(object asyncState, double n1, double n2)
+        {
+            Thread.Sleep(10000);//Ten Seconds
+            return (n1 + n2);
+        }
+
+        public IAsyncResult BeginSubtractMethod(double n1, double n2, AsyncCallback callback, object asyncState)
+        {
+            var task = Task<double>.Factory.StartNew((res) => MyBeginSubtractMethod(asyncState, n1, n2), asyncState);
+            return task.ContinueWith(res => callback(task));
+
+        }
+        public double EndSubtractMethod(IAsyncResult result)
+        {
+            return ((Task<double>)result).Result;
+        }
+
+        private double MyBeginSubtractMethod(object asyncState, double n1, double n2)
+        {
+            Thread.Sleep(10000);//Ten Seconds
+            return (n1 - n2);
+        }
+
+        public IAsyncResult BeginMultiplyMethod(double n1, double n2, AsyncCallback callback, object asyncState)
+        {
+            var task = Task<double>.Factory.StartNew((res) => MyBeginMultiplyMethod(asyncState, n1, n2), asyncState);
+            return task.ContinueWith(res => callback(task));
+
+        }
+        public double EndMultiplyMethod(IAsyncResult result)
+        {
+            return ((Task<double>)result).Result;
+        }
+
+        private double MyBeginMultiplyMethod(object asyncState, double n1, double n2)
+        {
+            Thread.Sleep(10000);//Ten Seconds
+            return (n1 * n2);
+        }
+
+        public IAsyncResult BeginDivideMethod(double n1, double n2, AsyncCallback callback, object asyncState)
+        {
+            var task = Task<double>.Factory.StartNew((res) => MyBeginDivideMethod(asyncState, n1, n2), asyncState);
+            return task.ContinueWith(res => callback(task));
+
+        }
+        public double EndDivideMethod(IAsyncResult result)
+        {
+            return ((Task<double>)result).Result;
+        }
+
+        private double MyBeginDivideMethod(object asyncState, double n1, double n2)
+        {
+            Thread.Sleep(10000);//Ten Seconds
+            return (n1 / n2);
+        }
+    
+    }
+}
---
+++ WCFServiceLibrary/ICalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract(Namespace="http://Microsoft.ServiceModel.Samples")]
+    public interface ICalculator
+    {
+        //IAsyncResult asynchronous pattern
+        [OperationContract(AsyncPattern = true)]
+        IAsyncResult BeginAddMethod(double n1, double n2, AsyncCallback callback, object asyncState);
+        double EndAddMethod(IAsyncResult result);
+
+        [OperationContract(AsyncPattern = true)]
+        IAsyncResult BeginSubtractMethod(double n1, double n2, AsyncCallback callback, object asyncState);
+        double EndSubtractMethod(IAsyncResult result);
+
+        [OperationContract(AsyncPattern = true)]
+        IAsyncResult BeginMultiplyMethod(double n1, double n2, AsyncCallback callback, object asyncState);
+
+        double EndMultiplyMethod(IAsyncResult result);
+
+        [OperationContract(AsyncPattern = true)]
+        IAsyncResult BeginDivideMethod(double n1, double n2, AsyncCallback callback, object asyncState);
+
+        double EndDivideMethod(IAsyncResult result);
+    }
+}
